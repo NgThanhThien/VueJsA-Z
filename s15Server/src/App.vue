ac16882798f58ba5b3ff7b5053e20600305bb817
Thien <thanhthienbk13@gmail.com>
2019-09-13 14:47:39 +0700

    finish s15 vue-resource

--- s15Server/src/App.vue
+++ s15Server/src/App.vue
@@ -1,60 +1,110 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 offset-md-3">
+        <h1 class="text-center">Vue Resource</h1>
+        <hr>
+        <input class="form-control"
+               type="text"
+               v-model="user.userName"
+               placeholder="User Name">
+        <input type="password"
+               class="form-control mt-md-3"
+               v-model="user.userPassword"
+               placeholder="Password">
+        <button class="btn btn-block btn-success mt-md-3" @click="submit">Submit</button>
+<!--        <hr>-->
+<!--        <button class="btn btn-block btn-outline-info mt-md-3" @click="getData">-->
+<!--          Get Data-->
+<!--        </button>-->
+        <hr>
+        <input type="text"
+               v-model="node"
+               class="form-control mt-md-3">
+        <button class="btn btn-block btn-outline-danger mt-md-3" @click="getUserData">
+          Get Data
+        </button>
+        <div class="card mt-md-3">
+          <div class="card-header text-center">Data</div>
+          <ul class="list-group">
+            <li class="list-group-item" v-for="user in userData">
+              {{ user.userName }} - {{ user.userPassword }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    export default {
+        data() {
+            return {
+                user: {
+                    userName: '',
+                    userPassword: ''
+                },
+                userData: [],
+                resource: {},
+                node: 'user'
+            }
+        },
+        methods: {
+            submit() {
+                // this.$http.post('', this.user)
+                //     .then(response => {
+                //         console.log(response);
+                //     }, error => {
+                //         console.log(error);
+                //     })
+                this.resource.createItem(this.user);
+            },
+            // getData() {
+            //     this.$http.get('')
+            //         .then(response => {
+            //             return response.json();
+            //         })
+            //         .then(data => {
+            //             for (let key in data) {
+            //                 this.userData.push(data[key])
+            //             }
+            //         })
+            // },
+            getUserData() {
+                this.resource.getNodeData({node: this.node})
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(data => {
+                        for (let key in data) {
+                            this.userData.push(data[key])
+                        }
+                    })
+            }
+        },
+        created() {
+            const customActions = {
+                createItem: {method: 'POST', url: 'user.json'},
+                getNodeData: {method: 'GET'}
+            }
+            this.resource = this.$resource('{node}.json', {}, customActions);
+        }
     }
-  }
-}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .box-user {
+    display: flex;
+    width: 100%;
+    margin: 10px;
+  }
 
-h1, h2 {
-  font-weight: normal;
-}
+  span.box-input {
+    width: 100%;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  .label-box {
+    width: 30%;
+  }
 </style>
