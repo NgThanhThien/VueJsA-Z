e039101f01ea11d7f0e3043e683eddba736cf96f
Thien <thanhthienbk13@gmail.com>
2019-09-13 15:04:16 +0700

    add s16 vueRouter

---
+++ s16Router/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+ <div class="container">
+   <div class="row">
+     <div class="col-md-6 offset-md-3">
+       <h1 class="text-center">Vue Router</h1>
+     </div>
+   </div>
+ </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style>
+
+</style>
